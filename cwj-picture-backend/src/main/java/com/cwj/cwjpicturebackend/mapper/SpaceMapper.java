333311f42922a9dd4da1dfb7d76152bffd9ad657
MaoPaIH <2365398049@qq.com>
2025-08-16 18:54:55 +0800

    空间模块

---
+++ cwj-picture-backend/src/main/java/com/cwj/cwjpicturebackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.cwj.cwjpicturebackend.mapper;
+
+import com.cwj.cwjpicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author eveni
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-08-16 14:28:45
+* @Entity com.cwj.cwjpicturebackend.model.entity.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
